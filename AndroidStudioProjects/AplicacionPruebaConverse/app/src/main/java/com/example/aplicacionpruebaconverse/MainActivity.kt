0cce6cf2324398e74fb2db5f27326b50e2491c71
jbulgon331 <148953231+joaquimon@users.noreply.github.com>
2024-09-30 08:56:30 +0200

    Proyectos Android Studio

---
+++ AndroidStudioProjects/AplicacionPruebaConverse/app/src/main/java/com/example/aplicacionpruebaconverse/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.aplicacionpruebaconverse
+
+import android.R
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.aplicacionpruebaconverse.ui.theme.AplicacionPruebaConverseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Text(
+                text = "Hola mundo",
+                color = Color.Green,
+                fontSize = 45.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
