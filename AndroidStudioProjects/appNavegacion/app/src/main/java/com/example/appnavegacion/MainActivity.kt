2616de3f7e4baf0b4120f8696c538c78dd474ced
jbulgon331 <148953231+joaquimon@users.noreply.github.com>
2024-11-07 12:16:07 +0100

    Clase 07/11
    Proyecto appNavegacion terminado

--- AndroidStudioProjects/appNavegacion/app/src/main/java/com/example/appnavegacion/MainActivity.kt
+++ AndroidStudioProjects/appNavegacion/app/src/main/java/com/example/appnavegacion/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
@@ -39,6 +41,7 @@ data class Contacto(
     val mail:String
 )
 
+//Creamos una lista mutable de 'Contacto'
 val contactos = mutableStateListOf<Contacto>(
     Contacto("JoseWi Bullon", "jlbulgon298@gmail.com"),
     Contacto("Paquito Bullon", "pbulgon436@gmail.com"),
@@ -46,7 +49,20 @@ val contactos = mutableStateListOf<Contacto>(
     Contacto("Furin Bullon", "fbulsar547@gmail.com")
 )
 
+//Tarjeta para los items
 @Composable
+fun Tarjeta(indice: Int, contacto: Contacto, navController: NavController){
+    ElevatedCard(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+    ) {
+        Row(modifier = Modifier) {  }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
 fun PantallaNavegacion(){
     Spacer(Modifier.size(40.dp))
     val navController = rememberNavController()
@@ -69,7 +85,34 @@ fun Pantalla1(navController: NavController){
     Spacer(Modifier.size(40.dp))
     Column {
         BarraBotones(navController = navController)
-        Text(text = "")
+        Text(text = "Tabla del 2")
+        for (x in 1..10){
+            Text("2 * $x = ${x * 2}")
+        }
+    }
+}
+
+@Composable
+fun Pantalla2(navController: NavController){
+    Spacer(Modifier.size(40.dp))
+    Column {
+        BarraBotones(navController = navController)
+        Text(text = "Tabla del 5")
+        for (x in 1..10){
+            Text("5 * $x = ${x * 5}")
+        }
+    }
+}
+
+@Composable
+fun Pantalla3(navController: NavController){
+    Spacer(Modifier.size(40.dp))
+    Column {
+        BarraBotones(navController = navController)
+        Text(text = "Tabla del 10")
+        for (x in 1..10){
+            Text("10 * $x = ${x * 10}")
+        }
     }
 }
 
@@ -77,7 +120,22 @@ fun Pantalla1(navController: NavController){
 fun BarraBotones(navController: NavController){
     Row(horizontalArrangement = Arrangement.SpaceAround,
         modifier = Modifier.fillMaxWidth()
-            .padding(10.dp)){
+            .padding(50.dp)){
+        Button(onClick = {
+            navController.navigate("pantalla1")
+        }) {
+            Text("Tabla 2")
+        }
+        Button(onClick = {
+            navController.navigate("pantalla2")
+        }) {
+            Text("Tabla 5")
+        }
+        Button(onClick = {
+            navController.navigate("pantalla3")
+        }) {
+            Text("Tabla 10")
+        }
 
         }
 }
